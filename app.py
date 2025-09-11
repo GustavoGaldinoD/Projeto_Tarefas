48c8fec7655ca13f3e1873bf6e8f2cd6d3a7358a
Gustavo Galdino dos Santos <2023105070028@aluno.ifro.edu.br>
2025-09-11 11:01:28 -0400

    upadate: etapa 3 - login e autenticacao

---
+++ app.py
@@ -0,0 +1,115 @@
+from flask import Flask, flash, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, current_user, login_use, login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+
+
+#configuraçao inicial
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sql:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_required.init_app(app)
+login_manager.login_view = 'login'
+
+#----------------------------------------------------
+#  MODELS
+#----------------------------------------------------
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.email(100), unique = True, nullable = True)
+    password = db.Column(db.String(20), nullable = False)
+    tasks = db.relationship('Task', backref = 'user', lazy = True)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(150))
+    status = db.Column(db.String(20, default = "Pendente"))
+    user_id = db.Column(db.Integer, db.ForeidnKey('user.id'), nullabel = False)
+
+#-------------------------------------------------
+#LOGIN MANAGER
+#-------------------------------------------------
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+#--------------------------------------------------
+# ROTAS
+#--------------------------------------------------
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+#cadastro de usuario == CREATE
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = generate_password_hash(request.form['password'])
+
+        #verificar se o usuario ja existe
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('email ja esta cadastrado', 'warning')
+            return redirect(url_for('register'))
+        
+        new_user = User(name=name, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('cadastro realizado com sucesso! faça login', 'sucesso')
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+#login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user) 
+            return redirect(url_for('tasks'))
+        else:
+            flash('Email ou senha incorreto', 'danger') 
+
+        return  render_template('login.html')   
+
+#logaut
+@app.route('/logaut')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+#listar tarefas --  READ
+@app.route
+@login_required
+def tasks():
+    user_tasks: Task.query.filter_by(user_id=current_user.id).all()
+    return render_template('tasks.html', tasks=user_tasks)
+
+
+ 
+
+#--------------------------------------------------
+# CRIAR BANCO NA PRIMEIRA EXECUÇAO
+#--------------------------------------------------
+
+
+if __name__ == '__main__':
+    if not os.path.exists("database.db"):
+        with app.app_context():
+            db.create_all()
+
+    app.run(debug=True)
